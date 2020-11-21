e754deee256b6811a09d31c8b10c83290385e00f
cesar <cesgomvar@gmail.com>
2020-11-21 11:59:00 -0600

    Cambios del boton

--- ProyectoProgra6/ProyectoProgra6/PantallaConfiguracion.Designer.cs
+++ ProyectoProgra6/ProyectoProgra6/PantallaConfiguracion.Designer.cs
@@ -36,7 +36,7 @@
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(280, 110);
+            this.button1.Size = new System.Drawing.Size(300, 121);
             this.button1.TabIndex = 0;
             this.button1.Text = "Configuracion";
             this.button1.UseVisualStyleBackColor = true;
