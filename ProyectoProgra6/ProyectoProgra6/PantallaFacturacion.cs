889dbfda63217e12c801e7983abfa595b7387f91
cesar <cesgomvar@gmail.com>
2020-11-21 11:46:04 -0600

    Se agrega pantalla de facturacion

---
+++ ProyectoProgra6/ProyectoProgra6/PantallaFacturacion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoProgra6
+{
+    public partial class PantallaFacturacion : Form
+    {
+        public PantallaFacturacion()
+        {
+            InitializeComponent();
+        }
+    }
+}
